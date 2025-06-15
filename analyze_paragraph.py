41f45d38baca9034cf700d0ac72dab07010d2268
aobozhang2025 <aobozhang2020@hotmail.com>
2025-06-16 07:42:10 +0800

    relative path

--- analyze_paragraph.py
+++ analyze_paragraph.py
@@ -13,14 +13,15 @@ if __name__ == "__main__":
 
     prompt = 'Imagine you are a chemist that is a professional in synthesizing catalysts, please summarize what catalyst is synthesized and the synthesis procedures with all the details from the following paragraph: '
 
-    files = Util.get_files_in_directory('D:\\pycharm-project\\Data_Processing\\paragraphs\\')
+    current_directory = os.getcwd()
+    directory = current_directory + '\\paragraphs\\'
+    files = Util.get_files_in_directory(directory)
 
     for file in files:
         result = Util.read_doi_paper_file(file)
         question = prompt + '\n\n' + result['content']
         reason, answer = deepseek.get_response(question)
 
-        current_directory = os.getcwd()
         file_name = current_directory + '\\answers\\' + result['doi'] + '.txt'
         to_write = 'question: \n\n' + question + '\n\nreasoning: \n\n' + reason + '\n\nanswer: \n\n' + answer
         Util.save_string_to_file(to_write, file_name)
